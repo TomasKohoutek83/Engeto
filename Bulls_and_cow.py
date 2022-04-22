e4605656b9fa887334a8c6bb78b12a008fe62198
Tom.K <tom.kokes@email.cz>
2022-04-22 22:36:11 +0200

    Bulls and cows

---
+++ Bulls_and_cow.py
@@ -0,0 +1,36 @@
+import random
+
+
+list_cisel = []
+for i in range(0,4):
+    n = random.randint (1,9)
+    list_cisel.append(n)
+for cislo in list_cisel:
+    if cislo.isdigit():
+        print("nok")
+
+
+
+
+
+cara = "-"*60
+
+
+
+print(f''' 
+Vitam te v nasi hre!
+{cara}
+Vygeneruji pro tebe 4 nahodna cisla, ktere musis uhadnout.
+Tato hra se jmenuje Bulls and Cows.
+{cara}''')
+int(input("Napis svuj typ (ctyrmistne cislo): "))
+
+print(f"{cara}")
+
+
+
+
+
+
+
+
