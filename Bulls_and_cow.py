c3bd8c596b38f44a900bc4180f463835e56d80bb
Tomas Kohoutek <102764837+TomasKohoutek83@users.noreply.github.com>
2022-05-17 14:15:34 +0200

    Update Bulls_and_cow.py

--- Bulls_and_cow.py
+++ Bulls_and_cow.py
@@ -56,6 +56,81 @@ print(tipovana_cisla)
 
 
 
+-------------- Druha verze ---------------------------------------------------------------------
+
+import random
+
+cara = "-"*60
+vybrana_cisla = []
+list_cisel = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+tipovana_cisla = []
+
+
+
+
+
+def gen_cisel():
+    while len(vybrana_cisla) != 4:
+        n = random.choice(list_cisel)
+        if n not in vybrana_cisla:
+            vybrana_cisla.append(n)
+            if vybrana_cisla[0] == 0:
+                vybrana_cisla.pop(0)
+                m = random.randint(1, 9)
+                vybrana_cisla.append(m)
+                
+                
+def tip_hrace():
+    cisla = input("Napis svuj tip (ctyrmistne cislo): ")
+    for x in cisla:
+        tipovana_cisla.append(x)
+    
+    
+    
+    
+
+
+def kontrola_tipu_1():
+    while True:
+        tip_hrace()
+        
+        if len(tipovana_cisla) != 4:
+            print("Nezadal jsi 4 cisla")
+        elif not tipovana_cisla.isdigit():
+            print("Nezadal jsi cisla")
+            
+        else:
+            break
+
+#def kontrola_nuly():
+
+
+
+
+
+
+
+
+
+
+print(f'''134
+Vitam te v nasi hre!
+{cara}
+Vygeneruji pro tebe 4 nahodna cisla, ktere musis uhadnout.
+Tato hra se jmenuje Bulls and Cows.
+{cara}''')
+
+gen_cisel()
+kontrola_tipu_1()
+#kontrola_nuly()
+
+
+print(f"{cara}")
+# print(vybrana_cisla)
+print(tipovana_cisla)
+
+
+
 
 
 
