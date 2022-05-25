fd3782cff0ba8bd22d3c404338a6dab60ce12cf4
Tomas Kohoutek <Tomas.kohoutek83@gmail.com>
2022-05-25 17:59:02 +0200

    ulozeni

--- Bulls_and_cow.py
+++ Bulls_and_cow.py
@@ -1,20 +1,20 @@
 import random
 
 cara = "-" * 60
-vybrana_cisla = []
+generovana_cisla = []
 list_cisel = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 tipovana_cisla = []
 
 
 def gen_cisel():
-    while len(vybrana_cisla) != 4:
+    while len(generovana_cisla) != 4:
         n = random.choice(list_cisel)
-        if n not in vybrana_cisla:
-            vybrana_cisla.append(n)
-            if vybrana_cisla[0] == 0:
-                vybrana_cisla.pop(0)
+        if n not in generovana_cisla:
+            generovana_cisla.append(n)
+            if generovana_cisla[0] == 0:
+                generovana_cisla.pop(0)
                 m = random.randint(1, 9)
-                vybrana_cisla.append(m)
+                generovana_cisla.append(m)
 
 
 def kontrola_tipu_1():
@@ -53,18 +53,21 @@ print(f'''
 Vitam te v nasi hre Bulls and Cows!
 {cara}
 Vygeneruji pro tebe 4 nahodna cisla, ktere musis uhadnout.
-Zadat 4 cisla ktera nesmi zacinat 0 a zadne cislo
-se nesmi opakovat
+Zadej 4 cisla ktera nesmi zacinat 0 a zadne cislo
+se nesmi opakovat.
 {cara}''')
+print()
 
 gen_cisel()
 kontrola_tipu_1()
 kontrola_nuly()
 kontrola_duplicit()
 
-
 print(f"{cara}")
-print(vybrana_cisla)
+
+
+
+
 print(tipovana_cisla)
 
 
