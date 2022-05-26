a4fd281be1a617ee43057bfd3ba44fc51d4d3e4a
Tomas Kohoutek <Tomas.kohoutek83@gmail.com>
2022-05-26 21:55:40 +0200

    ulozeni

--- Bulls_and_cow_2.py
+++ Bulls_and_cow_2.py
@@ -6,16 +6,46 @@ list_cisel = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 tipovana_cisla = []
 chyba = "Chybne zadani !!!"
 
+def gen_cisla():
+    while True:
+        cislo = random.randint(1000, 9999)
+        if kontrola_duplicit(cislo):
+            return cislo
 
-def gen_cisel():
-    while len(generovana_cisla) != 4:
-        n = random.choice(list_cisel)
-        if n not in generovana_cisla:
-            generovana_cisla.append(n)
-            if generovana_cisla[0] == 0:
-                generovana_cisla.pop(0)
-                m = random.randint(1, 9)
-                generovana_cisla.append(m)
+
+def kontrola_duplicit(cislo):
+    cisla_do_listu = prevod_cisel_list(cislo)
+    if len(cisla_do_listu) == len(set(cisla_do_listu)):
+        #print(cisla_do_listu)
+        return True
+    else:
+        return False
+
+def prevod_cisel_list(cislo):
+    return [int(i) for i in str(cislo)]
+
+
+def porovani_a_vysledek(cislo, tip):
+    bull_and_cow = [0, 0]
+    cisla_do_listu = prevod_cisel_list(cislo)
+    tip_do_listu = prevod_cisel_list(tip)
+
+# pouziti metody zip pro slozeni
+    for i, j in zip(cisla_do_listu, tip_do_listu):
+
+        # common digit present
+        if j in cisla_do_listu:
+
+            # common digit exact match
+            if j == i:
+                bull_and_cow[0] += 1
+
+            # common digit match but in wrong position
+            else:
+                bull_and_cow[1] += 1
+
+    return bull_and_cow
+
 
 
 def kontrola_tipu_1():
@@ -67,17 +97,15 @@ Zadej 4 cisla ktera nesmi zacinat 0 a zadne cislo
 se nesmi opakovat.
 {cara}''')
 
-gen_cisel()
-kontrola_tipu_1()
+gen_cisla()
+#kontrola_tipu_1()
 #kontrola_nuly()
 #kontrola_duplicit()
 
 print(f"{cara}")
 
-
-
-
-print(tipovana_cisla)
+# print(tipovana_cisla)
+# print(generovana_cisla)
 
 
 
