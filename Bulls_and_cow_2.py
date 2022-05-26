1da783f940255d73d022cf240523923739093a9d
Tomas Kohoutek <Tomas.kohoutek83@gmail.com>
2022-05-26 22:24:06 +0200

    ulozeni

--- Bulls_and_cow_2.py
+++ Bulls_and_cow_2.py
@@ -1,11 +1,10 @@
 import random
 
 cara = "-" * 60
-generovana_cisla = []
-list_cisel = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-tipovana_cisla = []
 chyba = "Chybne zadani !!!"
 
+
+# Funkce k vygenerovani hadaneho cisla
 def gen_cisla():
     while True:
         cislo = random.randint(1000, 9999)
@@ -16,7 +15,7 @@ def gen_cisla():
 def kontrola_duplicit(cislo):
     cisla_do_listu = prevod_cisel_list(cislo)
     if len(cisla_do_listu) == len(set(cisla_do_listu)):
-        #print(cisla_do_listu)
+        print(cisla_do_listu)
         return True
     else:
         return False
@@ -25,22 +24,20 @@ def prevod_cisel_list(cislo):
     return [int(i) for i in str(cislo)]
 
 
+
+#Funkce pro zjisteni bull and cow. Porovnani pozice mezi listy
 def porovani_a_vysledek(cislo, tip):
     bull_and_cow = [0, 0]
     cisla_do_listu = prevod_cisel_list(cislo)
     tip_do_listu = prevod_cisel_list(tip)
 
 # pouziti metody zip pro slozeni
-    for i, j in zip(cisla_do_listu, tip_do_listu):
+    for i, k in zip(cisla_do_listu, tip_do_listu):
 
-        # common digit present
-        if j in cisla_do_listu:
-
-            # common digit exact match
-            if j == i:
+        # zjisteni pozice a uprava promene bull_and_cow
+        if k in cisla_do_listu:
+            if k == i:
                 bull_and_cow[0] += 1
-
-            # common digit match but in wrong position
             else:
                 bull_and_cow[1] += 1
 
@@ -69,26 +66,6 @@ def kontrola_tipu_1():
                 tipovana_cisla.append(x)
                 break
 
-
-
-#def kontrola_nuly():
-    # prvni_cislo =int(tipovana_cisla[0])
-    # if prvni_cislo == 0:
-    #     print("Cislo zacina nulou!!")
-    #     tipovana_cisla.clear()
-    #     kontrola_tipu_1()
-
-#def kontrola_duplicit():
-    # for cislo in tipovana_cisla:
-    #     double = tipovana_cisla.count(cislo)
-    #     if double > 1:
-    #         print("Duplicita cisel!!")
-    #         tipovana_cisla.clear()
-    #         kontrola_tipu_1()
-
-
-
-
 print(f'''
 Vitam te v nasi hre Bulls and Cows!
 {cara}
@@ -97,15 +74,29 @@ Zadej 4 cisla ktera nesmi zacinat 0 a zadne cislo
 se nesmi opakovat.
 {cara}''')
 
-gen_cisla()
-#kontrola_tipu_1()
-#kontrola_nuly()
-#kontrola_duplicit()
+gen_cislo = gen_cisla()
+
+while True:
+    tip_hrace = int(input("Napis svuj tip: "))
+
+    if not kontrola_duplicit(tip_hrace):
+        print(f"{chyba} Tvuj tip obsahuje duplicitu !!!")
+        continue
+    if tip_hrace < 1000 or tip_hrace > 9999:
+        print(f"{chyba} Tvuj tip ma vice nez 4 cisla!!!")
+        continue
+
+    bull_cow = porovani_a_vysledek(gen_cislo, tip_hrace)
+    print(f"{bull_cow[0]} bulls, {bull_cow[1]} cows")
+
+
+    if bull_cow[0] == 4:
+        print("Dobra prace !!! Prisel jsi na to!!!")
+        break
 
 print(f"{cara}")
 
-# print(tipovana_cisla)
-# print(generovana_cisla)
+
 
 
 
